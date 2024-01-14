26c7cd8d214bd9b7190a9218e84e134c1cc6aa6b
ac2barreto <ac.barreto96@gmail.com>
2024-01-14 16:01:55 -0500

    clean and comment code

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,46 +1,44 @@
-// set the URL as a variable
+// Set the URL as a variable
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-
-// set different map layers and layout
-
-let str = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Set different map layers and layout
+let streetLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-let sat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+let satelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 15,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
 
 const baseMaps = {
-    Satellite: sat,
-    Street: str
+    Satellite: satelliteLayer,
+    Street: streetLayer
 };
 
 let myMap = L.map("map", {
     center: [0, 0],
     zoom: 2,
-    layers: [sat]
+    layers: [satelliteLayer]
 });
 
-
+// Function to determine color based on earthquake depth
 function getColor(eq_depth) {
-    return  eq_depth > 70 ? '#75272C' :
-            eq_depth > 50 ? '#C20612' :
-            eq_depth > 30 ? '#F50717' :
-            eq_depth > 10 ? '#F7515C' :
-            '#F7C3C4';
+    return eq_depth > 70 ? '#75272C' :
+           eq_depth > 50 ? '#C20612' :
+           eq_depth > 30 ? '#F50717' :
+           eq_depth > 10 ? '#F7515C' :
+           '#F7C3C4';
 }
 
-
+// Fetch earthquake data and plot circles on the map
 d3.json(url).then(function (data) {
     for (var i = 0; i < data.features.length; i++) {
-       
+        // Create circles with varying properties based on earthquake data
         L.circle([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]], {
             fillOpacity: 0.85,
             color: getColor(data.features[i].geometry.coordinates[2]),
-            radius: Math.pow(data.features[i].properties.mag, 2)*7500
+            radius: Math.pow(data.features[i].properties.mag, 2) * 7500
         })
         .bindPopup(`
             <ul style="list-style-type:none; padding: 0; font-size: 16px;">
@@ -50,12 +48,13 @@ d3.json(url).then(function (data) {
             </ul>
         `)
         .addTo(myMap);
-        
     }
 });
-const lgnd = L.control({ position: 'bottomright' });
 
-lgnd.onAdd = function () {
+// Create legend and add to the map
+const legendControl = L.control({ position: 'bottomright' });
+
+legendControl.onAdd = function () {
     var div = L.DomUtil.create('div', 'info legend'),
         depth = [0, 10, 30, 50, 70, 90];
 
@@ -67,6 +66,7 @@ lgnd.onAdd = function () {
 
     return div;
 };
-lgnd.addTo(myMap);
+legendControl.addTo(myMap);
 
+// Add layer control to the map
 L.control.layers(baseMaps).addTo(myMap);
